4ef924bf5a47526b6e89c454f04d74c93855eb4d
Luna Stadler <lu@papill0n.org>
2021-01-30 15:08:30 +0100

    Apply date range changes on updates
    This especially applies when `to` is set to "now" and thus needs an
    update every time.  Before this commit values later than that would
    simply not show.

--- static/board.js
+++ static/board.js
@@ -324,6 +324,9 @@ function createChart(name, config, global) {
       return d;
     }
 
+    myChart.options.scales.xAxes[0].ticks.min = toDate(config.from || global.defaults.from);
+    myChart.options.scales.xAxes[0].ticks.max = toDate(config.to || global.defaults.to);
+
     let u = new URL(`http://${global.defaults.source}/api/v1/query_range`);
     u.searchParams.set("query", config.query);
     let from = toDate(config.from || global.defaults.from);
